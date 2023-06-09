1aa069d902348eebb55c8fdb565c71f14c6072c3
therumi08 <86691639+therumi08@users.noreply.github.com>
2023-06-09 19:34:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+$("#ackapa").click(function() {
+$(".trwpwhatsappballon").toggle(1000);
+});
+$("#kapatac").click(function() {
+$(".trwpwhatsappballon").toggle(1000);
+});
+});
